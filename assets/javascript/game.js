650af7b61680742b8f73c00c9121c3fb10db0b24
Admin <admin@admins-mbp-2.lan>
2018-04-02 19:20:57 -0400

    Counter Attack values set with jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,12 +35,51 @@ function startGame() {
   var hpTheMountain = Math.floor(Math.random() * (125 - 50 + 1) + 50);
     $("#hpTheMountain").append(hpTheMountain);
     console.log(hpTheMountain);
-  //reset Attack Val
-//check win
 
+  //reset Attack values
+  var attackJon;
+  var attackDanny;
+  var attackDrogo;
+  var attackNightKing;
+  var attackTheHound;
+  var attackTheMountain;
+
+  //reset Counter values
+  var counterJon = Math.floor(Math.random() * (20 - 5 + 1) + 5);
+    $("#jonSnow").append("value", counterJon);
+  var counterDanny = Math.floor(Math.random() * (20 - 5 + 1) + 5);
+    $("#danny").append("value", counterDanny);
+  var counterDrogo = Math.floor(Math.random() * (20 - 5 + 1) + 5);
+    $("#drogo").append("value", counterDrogo);
+  var counterNightKing = Math.floor(Math.random() * (20 - 5 + 1) + 5);
+    $("#nightKing").append("value", counterNightKing);
+  var counterTheHound = Math.floor(Math.random() * (20 - 5 + 1) + 5);
+    $("#theHound").append("value", counterTheHound);
+  var counterTheMountain = Math.floor(Math.random() * (20 - 5 + 1) + 5);
+    $("#theMountain").append("value", counterTheMountain);
+
+  //return gameCharacters to 'your character' div
+
+
+
+//check win
+function checkWin() {
+
+};
 
 }
 
 startGame();
 
 });
+
+
+
+
+
+
+
+
+//notes:
+//have game know when a character is chosen
+//
