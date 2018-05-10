8fe8f22765a74d675cd1d8729696240ead2fa41f
Admin <mercerbe@gmail.com>
2018-05-10 17:40:32 -0400

    fixed nesting/refactoring so game functions on replay

--- assets/javascript/gameObj.js
+++ assets/javascript/gameObj.js
@@ -67,93 +67,95 @@ function startGame(){
         character.append(image);
         character.append("hp", health);
         characterDiv.append(character);
+
     }
 
-    function attack(a, b)
-    {
-        var damage = Math.floor(Math.random()*parseInt($(a).attr("attackPower")));
-        console.log("damage: " + damage);
-        var health = parseInt($(b).attr("health"));
-        health -= damage;
-        console.log("health: " + health);
-        $(b).attr("health",health);
-        $(b + " .health").text(health);
-        console.log("-----------------");
-    };
+  };
+  startGame();
+  function attack(a, b)
+  {
+      var damage = Math.floor(Math.random()*parseInt($(a).attr("attackPower")));
+      console.log("damage: " + damage);
+      var health = parseInt($(b).attr("health"));
+      health -= damage;
+      console.log("health: " + health);
+      $(b).attr("health",health);
+      $(b + " .health").text(health);
+      console.log("-----------------");
+  };
 
-    function combatantSelected()
-    {
-        if($("#defenders").html().trim() == "")
-        {
-            return false;
-        }
-        return true;
-    };
+  function combatantSelected()
+  {
+      if($("#defenders").html().trim() == "")
+      {
+          return false;
+      }
+      return true;
+  };
 
-    $("#characterSection").on("click", ".character", function() {
-        if(combatantSelected())
-        {
-            $(this).attr("id","attacker");
-            $(this).css("background-color", "rgb(241,0,73)");
-            $("#attackers").html(this);
-        }
-        else{
-            $(this).attr("id","defender");
-            $(this).css("background-color", "rgb(48,180,112)")
-            $("#defenders").html(this);
-        }
-      });
+  $("#characterSection").on("click", ".character", function() {
+      if(combatantSelected())
+      {
+          $(this).attr("id","attacker");
+          $(this).css("background-color", "rgb(241,0,73)");
+          $("#attackers").html(this);
+      }
+      else{
+          $(this).attr("id","defender");
+          $(this).css("background-color", "rgb(48,180,112)")
+          $("#defenders").html(this);
+      }
+    });
 
-    $("#buttons").on("click", "#attack", function() {
-        attack("#attacker","#defender");
-        attack("#defender","#attacker");
-        if(parseInt($("#defender").attr("health")) <= 0)
-        {
-            $("#defenders").html("");
-            defeatsCount++;
-            console.log("defeats: " + defeatsCount);
-            defeats.text(defeatsCount);
-            scoreCount -= 2;
-            console.log("score: " + scoreCount);
-            score.text(scoreCount);
-            $("#attackers").html("");
-            $("#defenders").html("");
-            $("#characterSection").html("");
-            startGame();
-        }
-        else if(parseInt($("#attacker").attr("health"))<= 0)
-        {
-            $("#attackers").html("");
-
-        }
-        if ($("#charactersSection").is(":empty")) {
-          winsCount++;
-          console.log("wins: " + winsCount);
-          wins.text(winsCount);
-          scoreCount += 5;
+  $("#buttons").on("click", "#attack", function() {
+      attack("#attacker","#defender");
+      attack("#defender","#attacker");
+      if(parseInt($("#defender").attr("health")) <= 0)
+      {
+          $("#defenders").html("");
+          defeatsCount++;
+          console.log("defeats: " + defeatsCount);
+          defeats.text(defeatsCount);
+          scoreCount -= 2;
           console.log("score: " + scoreCount);
           score.text(scoreCount);
           $("#attackers").html("");
           $("#defenders").html("");
           $("#characterSection").html("");
           startGame();
-        }
-    });
-    $("#buttons").on('click', '#reset', function() {
-      location.reload();
-    });
-    $("#buttons").on('click', '#retreat', function() {
-      defeatsCount++;
-      console.log("defeats: " + defeatsCount);
-      defeats.text(defeatsCount);
-      scoreCount -= 1;
-      console.log("score: " + scoreCount);
-      score.text(scoreCount);
-      $("#attackers").html("");
-      $("#defenders").html("");
-      $("#characterSection").html("");
-      startGame();
-    })
-  };
-  startGame();
+      }
+      else if(parseInt($("#attacker").attr("health"))<= 0)
+      {
+          $("#attackers").html("");
+
+      }
+      if ($("#charactersSection").is(":empty")) {
+        winsCount++;
+        console.log("wins: " + winsCount);
+        wins.text(winsCount);
+        scoreCount += 5;
+        console.log("score: " + scoreCount);
+        score.text(scoreCount);
+        $("#attackers").html("");
+        $("#defenders").html("");
+        $("#characterSection").html("");
+        startGame();
+      }
+  });
+  //reset and retreat btns
+  $("#buttons").on('click', '#reset', function() {
+    location.reload();
+  });
+  $("#buttons").on('click', '#retreat', function() {
+    defeatsCount++;
+    console.log("defeats: " + defeatsCount);
+    defeats.text(defeatsCount);
+    scoreCount -= 1;
+    console.log("score: " + scoreCount);
+    score.text(scoreCount);
+    $("#attackers").html("");
+    $("#defenders").html("");
+    $("#characterSection").html("");
+    startGame();
+  })
 });
